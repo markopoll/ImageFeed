dbe7ded40674f9320b55d722c1d30e3051172b65
Markopool <kuperlog@outlook.com>
2024-07-17 00:14:57 +0300

    реализовал тех задание. Учел требования по четности лайков, а также требования по оформлению

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -7,5 +7,12 @@
 
 import UIKit
 final class ImagesListCell: UITableViewCell {
+    // MARK: - IB Outlets
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    // MARK: - Public Properties
     static let reuseIdentifier = "ImagesListCell"
 }
--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -7,14 +7,22 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController  {
+final class ImagesListViewController: UIViewController  {
     // MARK: - IB Outlets
     @IBOutlet private var tableView: UITableView!
     
     // MARK: - Public Properties
-    let imagelist = ImagesListCell()
-    
     // MARK: - Private Properties
+    private let photosName: [String] = Array(0..<20).map { "\($0)" }
+   
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        formatter.locale = Locale(identifier: "ru_RU")
+        return formatter
+    } ()
+    
     // MARK: - Initializers
     // MARK: - Overrides Methods
     override func viewDidLoad() {
@@ -23,40 +31,65 @@ class ImagesListViewController: UIViewController  {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+       
     }
-    func configCell(for cell: ImagesListCell) { }
-    
-    
 }
-    // MARK: - IB Actions
-    // MARK: - Public Methods
-   
+// MARK: - IB Actions
+// MARK: - Public Methods
+
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-                
-                guard let imageListCell = cell as? ImagesListCell else {
-                    return UITableViewCell()
-                }
-                
-                configCell(for: imageListCell)
-                return imageListCell
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell, with: indexPath)
+        return imageListCell
     }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-      
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
     }
 }
 
-    // MARK: - Private Methods
-   
-    
-    
+extension ImagesListViewController {
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+        
+        cell.cellImage.image = image
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        
+        let isLiked = indexPath.row % 2 == 0
+        let likeImage = isLiked ? UIImage(named: "like_button_off") : UIImage(named: "like_button_on")
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
+}
+// MARK: - Private Methods
+
+
+
 
 
