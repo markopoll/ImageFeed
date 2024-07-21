8ea6ab4483856289c3f035e30509c3fdba9630d9
Markopool <kuperlog@outlook.com>
2024-07-21 21:14:18 +0300

    Создал второй контроллер, добавил лэйблы, аватарку и кнопку

---
+++ ImageFeed/ImageList/ProfileViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Mark Balikoti on 21.07.2024.
+//
+
+import UIKit
+final class ProfileViewController: UIViewController {
+    // MARK: - IB Outlets
+    
+    
+    // MARK: - Public Properties
+    // MARK: - Private Properties
+    // MARK: - Initializers
+    // MARK: - Overrides Methods
+    // MARK: - IB Actions
+    // MARK: - Public Methods
+    // MARK: - Private Methods
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    
+    
+}
